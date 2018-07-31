d0553bf4d26f3b92e25bfba78853f9223bca5fa9
Biswajit Das <bd.partha@gmail.com>
2018-07-31 15:09:52 +0600

    audio added

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.user.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+
+    }
+
+    public void backward(View view) {
+
+    }
+
+    public void pause(View view) {
+
+    }
+
+    public void forward(View view) {
+
     }
 }
